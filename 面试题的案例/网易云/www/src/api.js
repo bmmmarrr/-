ec1cdf3a8c5ea41f3acfcbcdde15df4495537c52
zhaokaixi <3062576176@qq.com>
2022-10-20 17:03:19 +0800

    网易云

---
+++ 面试题的案例/网易云/www/src/api.js
@@ -0,0 +1,4 @@
+import axios from 'axios'
+
+
+export const goods = () => axios.get(`http://localhost:3000/lyric?id=33894312`)
---
+++ 面试题的案例/网易云/www/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+
+import {goods} from "@/api";
+export default {
+  created() {
+    this.getdata();
+  },
+  methods: {
+    async getdata() {
+      let res = await goods();
+      console.log(res.data);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
